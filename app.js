433c243ed145b208c715775a03e2e329d4a2b045
Will Holcomb <will@dhappy.org>
2017-06-10 15:10:16 -0400

    working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('flapperNews', [])
+angular.module('CalendarApp', ['ngMaterial'])
     .controller('MainCtrl', [
 	'$scope',
 	function($scope) {
@@ -18,4 +18,9 @@ angular.module('flapperNews', [])
 		{title: 'post 4', upvotes: 9},
 		{title: 'post 5', upvotes: 4}
 	    ];
+
+	    $scope.addPost = function(){
+		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.title = '';
+	    };
 	}])

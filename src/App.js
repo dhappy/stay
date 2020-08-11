4c4837d1bd003054e129ad1fa5ee691304a880dc
Will Holcomb <dys@dhappy.org>
2020-08-10 21:51:48 -0400

    fixin' element animations 📚

--- src/App.js
+++ src/App.js
@@ -1,26 +1,145 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createRef } from 'react'
+import { TweenMax, Power3 } from 'gsap/all'
+import './App.css'
+import Property from './property.svg'
 
-function App() {
+export default () => {
+  const [doc, setDoc] = useState()
+  const [origBBox, setOrigBBox] = useState()
+  const svg = createRef()
+
+  const cleanAttributes = (attributes) => {
+    const attrs = {}
+    for(let attr of attributes) {
+      attrs[attr.nodeName] = attr.nodeValue
+    }
+
+    if(attrs.style) {
+      const style = {}
+      for(let elem of attrs.style.split(';')) {
+        let [prop, val] = elem.split(':')
+        prop = (
+          prop.split('-')
+          .map((part, i) => {
+            if(i === 0) {
+              return part
+            } else {
+              return part[0].toUpperCase() + part.slice(1)
+            }
+          })
+          .join('')
+        )
+        style[prop] = val
+      }
+      attrs.style = style
+    }
+
+    if(attrs.class) {
+      attrs.className = attrs.class
+      delete attrs.class
+    }
+    if(attrs['xml:space']) {
+      attrs.xmlSpace = attrs['xml:space']
+      delete attrs['xml:space']
+    }
+
+    return attrs
+  }
+
+  const buildTree = (root, key = 0) => {
+    if(root.nodeType !== Node.ELEMENT_NODE) {
+      console.error('Root Type', root.nodeType)
+    } else {
+      const children = []
+      for(let child of root.childNodes) {
+        if(child.nodeType === Node.ELEMENT_NODE) {
+          if([...child.childNodes].find(
+            sub => sub.nodeType !== Node.TEXT_NODE
+          )) {
+            children.push(buildTree(child, ++key))
+          } else {
+            const attrs = cleanAttributes(child.attributes)
+            attrs.key = ++key
+
+            const text = [...child.childNodes].map(c => c.data).join()
+            children.push(React.createElement(
+              child.nodeName, attrs, text
+            ))
+          }
+        } else if(child.data && child.data.trim() !== '') {
+          console.error('Child', child.data)
+        }
+      }
+      const attrs = cleanAttributes(root.attributes)
+      attrs.key = ++key
+
+      if(['parent', 'space'].includes(attrs.className)) {
+        const ref = createRef()
+        attrs.ref = ref
+        attrs.onClick = () => {
+          const current = svg.current.attributes.viewBox.nodeValue
+          if(!origBBox) {
+            setOrigBBox(current)
+          }
+          const bbox = ref.current.getBBox()
+
+          try {
+            
+            const tfm2elm = (
+              svg.current.getScreenCTM().inverse().multiply(ref.current.getScreenCTM())
+            )
+            const pad = 2
+            let origin = svg.current.createSVGPoint()
+            origin.x = bbox.x - pad
+            origin.y = bbox.y - pad
+            let dest = svg.current.createSVGPoint()
+            dest.x = origin.x + bbox.width + 2 * pad
+            dest.y = origin.y + bbox.height + 2 * pad
+            origin = origin.matrixTransform(tfm2elm)
+            dest = dest.matrixTransform(tfm2elm)
+            dest.x -= origin.x
+            dest.y -= origin.y
+            let newView = [origin.x, origin.y, dest.x, dest.y].join(' ')
+            if(current === newView) {
+              newView = origBBox
+            }
+            console.log(current, newView, origBBox)
+            TweenMax.to(
+              svg.current, 1, { attr: { viewBox: newView }, ease: Power3.easeInOut }
+            )
+          } catch(e) {
+            console.error(e)
+          }
+        }
+      }
+
+      if(root.nodeName === 'svg') {
+        attrs.ref = svg
+      }
+
+      return React.createElement(
+        root.nodeName, attrs, children
+      )
+    }
+  }
+
+  const loadDoc = async () => {
+    const res = await fetch(Property)
+    const doc = await res.text()
+    setDoc(doc)
+  }
+
+  useEffect(() => { loadDoc() }, [])
+
+  let SVG
+  if(doc) {
+    const dom = (new DOMParser()).parseFromString(doc, 'text/xml')
+    SVG = buildTree(dom.documentElement)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{height: '100vh'}}>
+      {SVG}
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/SVGRTest.jsx
@@ -0,0 +1,367 @@
+import * as React from "react"
+
+function SvgComponent(props) {
+  return (
+    <svg viewBox="-17 -11 124.541 72.986" {...props}>
+      <title>{"Riddle Creek"}</title>
+      <defs>
+        <marker
+          orient="auto"
+          refY={0}
+          refX={0}
+          id="prefix__a"
+          overflow="visible"
+        >
+          <path
+            d="M-1.2 0l-1 1 3.5-1-3.5-1 1 1z"
+            fillRule="evenodd"
+            stroke="#000"
+            strokeWidth=".2pt"
+          />
+        </marker>
+      </defs>
+      <path
+        d="M-1.477 32.154v15h67.755l.075-15z"
+        fill="#d7d7d7"
+        stroke="#000009"
+        strokeWidth={0.25}
+      />
+      <text
+        style={{
+          InkscapeFontSpecification: "Candara",
+        }}
+        x={18.651}
+        y={35.574}
+        fontSize={32}
+        fontWeight={400}
+        fontFamily="Candara"
+        transform="translate(-.977 2.686)"
+      />
+      <path
+        d="M66.572 12.727h40.87c-.723 21.295 1.924 30.906-5.322 39.623-6.66 8.012-35.661 9.51-35.661 9.51z"
+        fill="#826e4e"
+        stroke="#000"
+        strokeWidth={0.2}
+      />
+      <g transform="translate(-.977 2.686)">
+        <path
+          d="M-9.203-13.597c1.156-.156 3.057.574 3.36.903-.348.712-.828 1.363-1.133 1.88-.304.516-.557 1.049-.753 1.382-.195.333-.373.541-.512.783-.139.243-.221.506-.327.695-.105.19-.289.344-.332.447-.043.103.068.137.024.192-.044.055-.352.044-.318.096.3.461-.01-.114-.01.353"
+          id="prefix__b"
+          fill="#b3370c"
+          fillOpacity={1}
+          stroke="#000"
+          strokeWidth={0.118}
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeMiterlimit={4}
+          strokeOpacity={1}
+          strokeDasharray="none"
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__c"
+          xlinkHref="#prefix__b"
+          y={0}
+          x={0}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__d"
+          xlinkHref="#prefix__c"
+          y={0}
+          x={0}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__e"
+          xlinkHref="#prefix__d"
+          y={0}
+          x={0}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__f"
+          xlinkHref="#prefix__e"
+          y={0}
+          x={0}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__g"
+          xlinkHref="#prefix__f"
+          y={0}
+          x={0}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__h"
+          xlinkHref="#prefix__g"
+          y={0}
+          x={0}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__i"
+          xlinkHref="#prefix__h"
+          y={0}
+          x={0}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__j"
+          xlinkHref="#prefix__i"
+          y={0}
+          x={0}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__k"
+          xlinkHref="#prefix__j"
+          y={0}
+          x={0}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          id="prefix__l"
+          xlinkHref="#prefix__k"
+          y={0}
+          x={0}
+          fill="#ac350c"
+          fillOpacity={0.851}
+        />
+        <use
+          height={1052.362}
+          width={744.094}
+          transform="rotate(30 -9.203 -6.866)"
+          xlinkHref="#prefix__l"
+        />
+      </g>
+      <g>
+        <path d="M-1.727 17.31l-11.75.25v34.626" fill="none" stroke="#000dd0" />
+        <rect
+          width={6.187}
+          height={8.839}
+          x={-14.039}
+          y={6.363}
+          rx={1}
+          ry={1}
+          strokeWidth={0.3}
+          transform="translate(-2.558 46.634)"
+          fill="#0023e3"
+          fillOpacity={0.093}
+          stroke="#000552"
+          strokeOpacity={0.633}
+        />
+        <g fill="#1524d7" fillOpacity={0.303} stroke="#3c3f74">
+          <path
+            d="M-11.639 61.267l-.006-2.78c0-.17-.097-.253-.202-.253h-.977c-.054 0-.054 0-.054-.048v-.953c0-.041.135 0 .135-.049v-1.04c0-.048 0-.048.063-.048h.807c.117 0 .117 0 .117.1 0 .472 0 .472.016.472h.49c.012 0 .012-.129.012-.572 0-.443-.007-.533-.137-.533h-1.32c-.035 0-.048 0-.048-.053v-1.698c0-.332 0-.332-.08-.332h-.965c-.082 0-.082.009-.082.05 0 .338.082.21-.358.532-.036.026-1.532.903-1.567.923-.342-.047-.412.325-.31.544.104.22.563.251.619-.148.041-.021 1.193-.74 1.258-.773.1-.051.358-.316.358.168v2.408c0 .049.134 0 .134.049v.953c0 .048 0 .048-.046.048h-.894c-.144 0-.247.16-.247.253v2.78c0 .099.077.167.143.167h3.024c.091 0 .117-.094.117-.167z"
+            strokeWidth={0.10311}
+          />
+          <path
+            d="M-11.256 57.883c-.044.09-.386.035-.38-.28.004-.172.148-.325.172-.452.015-.075-.056-.188-.076-.26.105.093.19.088.365.258.083.082.15.203.138.336-.02.249-.273.272-.264.244.081-.024.148-.1.188-.203.081-.215-.165-.402-.319-.515.11.262-.086.337-.124.592-.025.166.094.326.3.28z"
+            fillRule="evenodd"
+            strokeWidth={0.012906879999999997}
+          />
+          <path
+            d="M-11.256 57.883c-.044.09-.386.035-.38-.28.004-.172.148-.325.172-.452.015-.075-.056-.188-.076-.26.105.093.19.088.365.258.083.082.15.203.138.336-.02.249-.273.272-.264.244"
+            fillRule="evenodd"
+            strokeWidth={0.012906879999999997}
+          />
+        </g>
+      </g>
+      <g>
+        <path
+          d="M-1.472 16.14c-7.148 0-2.15-12.414-6.692-12.412l-6.672.003"
+          fill="none"
+          stroke="red"
+          strokeWidth={0.3}
+        />
+        <g transform="translate(-3.684 -2.648)">
+          <rect
+            width={4.596}
+            height={4.906}
+            x={-12.448}
+            y={7.291}
+            rx={1}
+            ry={1}
+            fill="#e30018"
+            fillOpacity={0.093}
+            stroke="#360000"
+            strokeWidth={0.3}
+            strokeOpacity={0.514}
+          />
+          <g transform="matrix(.13198 0 0 .13198 -38.12 -2.195)">
+            <path
+              d="M198.486 84.894v-4.891m0 0l.047 4.738c.046 2.492 1.985 4.636 4.377 4.636 2.391 0 4.33-2.144 4.33-4.79 0-1.27-.456-2.487-1.259-3.383-1.68 1.87-1.702 4.893-.016 6.764 1.688 1.874 4.43 1.88 6.124.012 1.694-1.867 1.7-4.899-.048-6.775-1.748 1.864-1.75 4.892-.064 6.763 1.688 1.874 4.43 1.88 6.124.012 1.694-1.867 1.699-4.899.003-6.775-.82.895-1.277 2.113-1.277 3.383 0 2.645 1.94 4.789 4.33 4.789 2.392 0 4.331-2.144 4.331-4.636v-4.738"
+              id="prefix__m"
+              fill="none"
+              stroke="#000"
+              strokeWidth={0.624}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeMiterlimit={4}
+              strokeOpacity={0.961}
+              strokeDasharray="none"
+              strokeDashoffset={0}
+            />
+            <use
+              height={28.034}
+              width={27.626}
+              transform="matrix(1 0 0 -1 0 181.93)"
+              xlinkHref="#prefix__m"
+            />
+          </g>
+        </g>
+      </g>
+      <g>
+        <g transform="translate(-.977 2.686)">
+          <path
+            d="M2.812 15.024v12.883h-3.845v-2.131h-4.225v-8.752h4.133v-2h3.937z"
+            id="prefix__n"
+            fill="#ff8f53"
+            stroke="#0a0904"
+            strokeWidth={0.15}
+            strokeLinejoin="round"
+            strokeMiterlimit={4}
+            strokeOpacity={1}
+            display="inline"
+          />
+        </g>
+        <path
+          fill="#ffdcbd"
+          fillOpacity={0.936}
+          stroke="#000009"
+          strokeWidth={0.1}
+          d="M47.528 13.14h17v1h-17zM36.528 2.14h4v1h-4zM47.523-6.846v9h-3v1h3v6h1v-3h3v-3h-1v2h-2v-12h-1zM47.523 18.154v7h5v6h1v-6h3v-7h-3v1h2v5h-7v-6h-1zM61.528 24.14h3v1h-3zM58.528 28.14h1v3h-1zM50.528-6.86h1v4h-1z"
+        />
+        <path
+          d="M64.102 30.62l-.046-36.983-63.973-.02L.08 21.605l-4.158.005-.005 4.964 4.26-.011v4.044zm-66.107-2.15l-4.243.001-.011-8.77 4.163-.017.002-27.96 68.269-.005v40.79l-68.173.016z"
+          fill="#cbc7c3"
+          stroke="#23201f"
+          strokeWidth={0.1}
+          strokeLinejoin="round"
+        />
+        <path fill="#cbc7c3" d="M66.116-7.343v1.156h-2.01v-1.156z" />
+        <g
+          fill="#bdcdff"
+          fillOpacity={0.936}
+          stroke="#000009"
+          strokeWidth={0.117}
+          strokeLinejoin="round"
+        >
+          <path d="M3.528 30.328h4v2.406h-4zM24.528 30.39h4v2.313h-4zM31.528 30.453h5v2.25h-5zM39.528 30.42h4v2.282h-4zM54.528 30.42h3v2.25h-3zM63.87 15.14h2.47v4h-2.47zM-2.222 1.14H.216v9h-2.438z" />
+        </g>
+        <g
+          fill="#845530"
+          stroke="#000"
+          strokeWidth={0.1}
+          strokeLinejoin="round"
+        >
+          <path d="M12.528 31.14h9v1h-9zM46.528 31.14h5v1h-5zM55.528 14.14h1v4h-1zM40.528 2.14h4v1h-4zM50.528-2.86h1v6h-1zM47.532 9.252h1v3.8h-1z" />
+        </g>
+        <g
+          fill="#8a3e09"
+          fillOpacity={0.936}
+          stroke="#000009"
+          strokeWidth={0.3}
+        >
+          <path d="M9.528 2.14h3v8h-3zM2.528-3.86v17h-3v-20h9v3z" />
+        </g>
+        <g>
+          <path fill="#f0e2c0" d="M25.528-6.86h2v6h-2z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M25.528-6.86h2v6h-2z"
+          />
+          <path fill="#f0e2c0" d="M27.528-6.86h2v6h-2z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M27.528-6.86h2v6h-2z"
+          />
+          <path fill="#f0e2c0" d="M29.528-6.86h2v6h-2z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M29.528-6.86h2v6h-2z"
+          />
+          <path fill="#f0e2c0" d="M31.528-6.86h5v6h-5z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M31.528-6.86h5v6h-5z"
+          />
+          <path fill="#f0e2c0" d="M31.528-.86h5v2h-5z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M31.528-.86h5v2h-5z"
+          />
+          <path fill="#f0e2c0" d="M31.528 1.14h5v2h-5z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M31.528 1.14h5v2h-5z"
+          />
+          <path fill="#f0e2c0" d="M31.528 3.14h5v2h-5z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M31.528 3.14h5v2h-5z"
+          />
+          <path fill="#f0e2c0" d="M31.528 5.14h5v2h-5z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M31.528 5.14h5v2h-5z"
+          />
+          <path fill="#f0e2c0" d="M31.528 9.14h5v2h-5z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M31.528 9.14h5v2h-5z"
+          />
+          <path fill="#f0e2c0" d="M31.528 7.14h5v2h-5z" />
+          <path
+            fill="none"
+            stroke="#63462b"
+            strokeWidth={0.1}
+            d="M31.528 7.14h5v2h-5z"
+          />
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export default SvgComponent

c0994072495cb97e3fbb720fc3556ebbcbeb9710
Will Holcomb <dys@dhappy.org>
2020-08-16 03:16:15 -0400

    Updates

---
+++ precache-manifest.0668ba3df565c1370e7a7a3df6546ad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14ded604d79919aa91c591e58a5b511",
+    "url": "/stay/index.html"
+  },
+  {
+    "revision": "808ab3981148bbccbf9f",
+    "url": "/stay/static/css/main.21c83fd0.chunk.css"
+  },
+  {
+    "revision": "5cbd1880189a70d0ef69",
+    "url": "/stay/static/js/2.c37c2ffc.chunk.js"
+  },
+  {
+    "revision": "2206f6f6cbbf99cae08d01b2952f71ad",
+    "url": "/stay/static/js/2.c37c2ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808ab3981148bbccbf9f",
+    "url": "/stay/static/js/main.400f4d39.chunk.js"
+  },
+  {
+    "revision": "62f1a863338aea14f84f",
+    "url": "/stay/static/js/runtime-main.a57ee21c.js"
+  }
+]);
